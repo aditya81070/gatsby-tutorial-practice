730f9d16f568e39af9491f878c290b0cd8d55b92
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-05-02 19:57:03 +0530

    complete tutorial of gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,21 @@
 module.exports = {
-  plugins: [{
-    resolve: `gatsby-plugin-typography`,
-    options: {
-      pathToConfigModule: `src/utils/typography`
-    }
-  }]
+  siteMetadata: {
+    title: `Pandas Eating Lots`
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`
+      }
+    }]
 }
